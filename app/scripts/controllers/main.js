4e20092f1c31d9a01e683539a0fff6d1ec7368f8
IvanCoronado <ivacormor@gmail.com>
2015-07-22 17:12:35 +0200

    yeoman stuff removed

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,5 @@
  */
 angular.module('dashboardApp')
   .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+   
   });
