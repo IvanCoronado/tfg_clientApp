ceacb29a1222bfb6fbfe01589148d52b1186c887
IvanCoronado <ivacormor@gmail.com>
2015-07-22 17:04:34 +0200

    yo angular dashboard

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name dashboardApp
+ * @description
+ * # dashboardApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('dashboardApp', []);
